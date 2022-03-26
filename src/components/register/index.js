21b34e97f079c9b7976bfd180342ae25ae3e9fc8
RayanBallout <rayanballout14@gmail.com>
2022-03-26 20:09:14 +0200

    Fixing css and adding email input

--- src/components/register/index.js
+++ src/components/register/index.js
@@ -8,35 +8,48 @@ const Register = () => {
 			<div className="register-card">
 				<h1>Register</h1>
 				<form className="register-form">
-					<label htmlFor="first_name">First Name:</label>
+					<label htmlFor="first_name">First Name:*</label>
 					<input
 						type="text"
 						id="first_name"
 						placeholder="Please enter your first name"
+						required
 					/>
-					<label htmlFor="last_name">Last Name:</label>
+					<label htmlFor="last_name">Last Name:*</label>
 					<input
 						type="text"
 						id="last_name"
 						placeholder="Please enter your last name"
+						required
 					/>
-					<label htmlFor="username">Username:</label>
+					<label htmlFor="email">Email:*</label>
+					<input
+						type="email"
+						id="email"
+						placeholder="Please enter your email"
+						required
+					/>
+					<label htmlFor="username">Username:*</label>
 					<input
 						type="text"
 						id="username"
 						placeholder="Please enter a username"
+						required
 					/>
-					<label htmlFor="password">Password:</label>
+
+					<label htmlFor="password">Password:*</label>
 					<input
 						type="password"
 						id="password"
 						placeholder="Please enter your password"
+						required
 					/>
-					<label htmlFor="password">Confirm Password:</label>
+					<label htmlFor="password">Confirm Password:*</label>
 					<input
 						type="password"
 						id="password"
 						placeholder="Please confirm your password"
+						required
 					/>
 					<input type="submit" className="submit" value="Register" />
 				</form>
