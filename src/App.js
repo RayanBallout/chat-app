a53c3267cdb8c185f95ce98d768afde5bf1f3608
RayanBallout <rayanballout14@gmail.com>
2022-03-27 20:49:21 +0300

    Setting up the routes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,66 @@
 import "./App.css"
 
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 
 import Login from "./components/Login"
 import Register from "./components/register"
 import ChatComponent from "./components/ChatComponent"
 import { useState } from "react"
-import { GeneralContext } from "./context/generalContext"
 import AddUser from "./components/AddUser"
 import Settings from "./components/Settings"
+import { useAuthContext } from "./hooks/useAuthContext"
+import { GeneralContext } from "./context/generalContext"
 
 function App() {
 	const [addUserActive, setAddUserActive] = useState(false)
 	const [chatIsOpen, setChatIsOpen] = useState(true)
 	const [settingsIsOpen, setSettingsIsOpen] = useState(false)
 
+	const { user, authIsReady } = useAuthContext()
+
 	return (
 		<div className="App">
-			<GeneralContext.Provider
-				value={{ setAddUserActive, setSettingsIsOpen, setChatIsOpen }}
-			>
-				<BrowserRouter>
-					<Routes>
-						<Route path="/" element={<Login />} />
-						<Route path="/register" element={<Register />} />
-						<Route
-							path="/chats"
-							element={<ChatComponent chatIsOpen={chatIsOpen} />}
-						/>
-					</Routes>
-				</BrowserRouter>
-				<AddUser isActive={addUserActive} />
-				<Settings isActive={settingsIsOpen} />
-			</GeneralContext.Provider>
+			{authIsReady && (
+				<GeneralContext.Provider
+					value={{
+						setAddUserActive,
+						setSettingsIsOpen,
+						setChatIsOpen,
+					}}
+				>
+					<BrowserRouter>
+						<Routes>
+							<Route
+								exact
+								path="/"
+								element={
+									user ? (
+										<ChatComponent
+											chatIsOpen={chatIsOpen}
+										/>
+									) : (
+										<Navigate to="/login" />
+									)
+								}
+							/>
+							<Route
+								exact
+								path="/login"
+								element={user ? <Navigate to="/" /> : <Login />}
+							/>
+							<Route
+								exact
+								path="/register"
+								element={
+									user ? <Navigate to="/" /> : <Register />
+								}
+							></Route>
+						</Routes>
+					</BrowserRouter>
+					<AddUser isActive={addUserActive} />
+					<Settings isActive={settingsIsOpen} />
+				</GeneralContext.Provider>
+			)}
 		</div>
 	)
 }
