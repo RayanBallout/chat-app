e7c7a78abb32133d31f041500709369d014e0f5e
RayanBallout <rayanballout14@gmail.com>
2022-03-27 12:40:51 +0300

    Adding the minimum styling

--- src/components/ProfileBar/index.js
+++ src/components/ProfileBar/index.js
@@ -1,7 +1,20 @@
 import React from "react"
+import userImage from "../../assets/images/user.jpg"
+import settingsIcons from "../../assets/icons/settings.svg"
+import "./profileBar.css"
 
 const ProfileBar = () => {
-	return <div className="profile-bar-container">profile bar</div>
+	return (
+		<div className="profile-bar-container">
+			<div className="info-container">
+				<img src={userImage} className="user-image" alt="user image" />
+				<p>Rayan Ballout</p>
+			</div>
+			<div className="settings-container">
+				<img src={settingsIcons} alt="settings" />
+			</div>
+		</div>
+	)
 }
 
 export default ProfileBar
