575012f94022c3a7334e3402876611a5e89dabc4
RayanBallout <rayanballout14@gmail.com>
2022-03-27 15:44:45 +0300

    Finishing the input bar

--- src/components/ChatModule/LowerBar.js
+++ src/components/ChatModule/LowerBar.js
@@ -1,8 +1,22 @@
 import React from "react"
 import "./chatModule.css"
+import sendIcon from "../../assets/icons/send.svg"
 
 const LowerBar = () => {
-	return <div className="lower-bar">LowerBar</div>
+	return (
+		<div className="lower-bar">
+			<form className="lower-bar-form">
+				<input
+					type="text"
+					className="text-input"
+					placeholder="Enter text message"
+				/>
+				<button className="send-message">
+					<img src={sendIcon} alt="send" className="send-image" />
+				</button>
+			</form>
+		</div>
+	)
 }
 
 export default LowerBar
